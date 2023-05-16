3b85e60e70dd434e821d70520dcb74fbcf3d8adf
DeanDodds <deandodds88@gmail.com>
2023-05-16 21:31:28 +0000

    code brokeng up in. to component files

---
+++ my-app/src/books.js
@@ -0,0 +1,32 @@
+export const books = [
+    {
+        id: 1,
+        author: 'Lucinda Riley',
+        title: 'Atlas: The Story of Pa Salt: The epic conclusion to the Seven Sisters series',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/81iw2hmh35L._AC_UL900_SR900,600_.jpg',
+    },
+    {
+        author: 'Jessie Inchauspé',
+        title: 'The Glucose Goddess Method: Your four-week guide to cutting cravings, getting your energy back, and feeling amazing. With 100+ super easy recipes',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/81dL+4lDWzL._AC_UL900_SR900,600_.jpg',
+        id: 2,
+    },
+    {
+        id: 3,
+        author: 'Ronnie O Sullivan',
+        title: 'Unbreakable',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/51xbeUPzfiL._AC_UL900_SR900,600_.jpg',
+    },
+    {
+        id: 4,
+        author: 'Richard Osman',
+        title: 'The Bullet That Missed: (The Thursday Murder Club 3)',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/71VZHVFiywL._AC_UL900_SR900,600_.jpg',
+    },
+    {
+        id: 5,
+        author: 'Nathan Anthony',
+        title: 'Bored of Lunch: The Healthy Air Fryer Book',
+        img: 'https://images-eu.ssl-images-amazon.com/images/I/61e1n99gzqL._AC_UL900_SR900,600_.jpg',
+    }
+]
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,58 +1,26 @@
 import React from 'react';
-import ReactDOM  from 'react-dom/client';
-import './index.css'
+import ReactDOM from 'react-dom/client';
 
-const books = [
-    {
-        id: 1,
-        author: 'Lucinda Riley',
-        title: 'Atlas: The Story of Pa Salt: The epic conclusion to the Seven Sisters series',
-        img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg',
-    },
-    {
-        author: 'Jessie Inchauspé',
-        title: 'The Glucose Goddess Method: Your four-week guide to cutting cravings, getting your energy back, and feeling amazing. With 100+ super easy recipes',
-        img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg ',
-        id: 2,
-    },
-    {
-        id: 3,
-        author: 'Ronnie O Sullivan',
-        title: 'Unbreakable',
-        img: 'https://m.media-amazon.com/images/I/51xbeUPzfiL._SY291_BO1,204,203,200_QL40_ML2_.jpg',
-    },
-    {
-        id: 4,
-        author: 'Richard Osman',
-        title: 'The Bullet That Missed: (The Thursday Murder Club 3)',
-        img: 'https://m.media-amazon.com/images/I/41sNio+MT3L._SY344_BO1,204,203,200_.jpg',
-    }
-]
+import './index.css';
 
+import { books } from './books';
+import Book from './Book';
 
-const BookList = () => {
-    return (
-    <section className='booklist'>
+function BookList() {
+  return (
+    <>
+      <h1>amazon best sellers</h1>
+      <section className='booklist'>
         {books.map((book) => {
-            return <Book {...book} key={book.id}/>;
-})}
-    </section>)
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Book = (props) =>{
-    const { img, title, author } = props;
-    return (
-        <article className='book'>
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-        </article>
-    )
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BookList/>)
+root.render(<BookList />);
 
 
